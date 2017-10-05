37a1498ca02b4bccbf7fd3153c0bac179743fb93
Luis Dipotet Garabito <ldipotet.job@gmail.com>
2017-10-05 13:46:40 +0100

    adding documentation

--- modules/apirest/app/views/Csv.scala
+++ modules/apirest/app/views/Csv.scala
@@ -16,14 +16,25 @@ import play.twirl.api._
 import scala.collection.immutable
 import scala.collection.immutable.Seq
 
+// The `Csv` result type. We extend `BufferedContent[Csv]` rather than just `Appendable[Csv]` so
+// Play knows how to make an HTTP result from a `Csv` value
 
+/**
+  * Cvs let us to build the csv result  and at the same time how serialize it as an HTTP response.
+  *
+  * In our case A is Csv
+  *
+  * @see play.twirl.api.BufferedContent[A]
+  * @see play.twirl.api.Appendable[A]
+  * @see play.twirl.api.Content
+  *
+  */
 class Csv(buffer: immutable.Seq[Csv],text:String,escape:Boolean) extends BufferedContent[Csv](buffer, text) {
 
   val contentType = Csv.contentType
 
   def this(text: String) = this(Nil, Formats.safe(text),false)
   def this(buffer: immutable.Seq[Csv]) = this(buffer, "",false)
-
 
   override protected def buildString(builder: StringBuilder) {
     if (elements.nonEmpty) {
@@ -45,6 +56,8 @@ class Csv(buffer: immutable.Seq[Csv],text:String,escape:Boolean) extends Buffere
 
 
 object Csv {
+
+  //this is the content-type the will be returned with the Play HTTP Response
   val contentType = "text/csv"
   implicit def contentTypeCsv(implicit codec: Codec): ContentTypeOf[Csv] = ContentTypeOf[Csv](Some(Csv.contentType))
 
@@ -53,7 +66,14 @@ object Csv {
   def empty: Csv = new Csv("")
 }
 
+/**
+  *
+  * twirl engine need to know how integrate the template parts, what have to render in OUR case Cvs.
+  * So we need to define when we will scape a content and when that content will be in raw.
+  *
+  */
 object CsvFormat extends Format[Csv] {
+
   def raw(text: String): Csv = Csv(text)
   def escape(text: String): Csv = {
     new Csv(Nil, text, true)
