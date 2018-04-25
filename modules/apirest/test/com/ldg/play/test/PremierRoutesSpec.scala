ef98bc4844bae5e2c6b0e403de28c39583bc0b6e
Luis Dipotet Garabito <ldipotet.job@gmail.com>
2018-04-26 00:05:29 +0100

    test route file

--- modules/apirest/test/com/ldg/play/test/PremierRoutesSpec.scala
+++ modules/apirest/test/com/ldg/play/test/PremierRoutesSpec.scala
@@ -1,21 +1,26 @@
 package com.ldg.play.test
 
 import akka.stream.Materializer
-
+import apirest.Routes
 import org.scalatest.{FlatSpec, Matchers}
+import play.api.inject.bind
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-
 import play.api.inject.guice.GuiceApplicationBuilder
+import services.{MockTDataServices, TDataServices}
 
 class PremierRoutesSpec extends FlatSpec with Matchers {
 
-  val app: play.api.Application = new GuiceApplicationBuilder().build()
-  implicit lazy val materializer: Materializer = app.materializer
+  val app = new GuiceApplicationBuilder()
+    .overrides(bind[TDataServices].to[MockTDataServices])
+    .configure("play.http.router" -> classOf[Routes].getName)
+    .build()
+
+ // implicit lazy val materializer: Materializer = app.materializer
 
    "Request /GET/premier/PRML/matchs with Content-Type:text/plain and application/json" should
     "return a json file Response with a 200 Code" in {
-    val Some(result) = route(app, FakeRequest(GET, "/apirest/premier/matchs")
+    val Some(result) = route(app, FakeRequest(GET, "/premier/matchs")
       .withHeaders(("Accept", "application/json"),("Content-Type", "text/plain")))
     status(result) shouldBe OK
   }
